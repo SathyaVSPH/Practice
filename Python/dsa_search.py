4ca236a1e30a0fd308bf57b28dfcc943210dcf4b
AlienInvasion <vijayvsph@gmail.com>
2025-07-08 16:23:00 +0530

    added sorted function in init; interpolation search. edited search.py

--- Python/dsa_search.py
+++ Python/dsa_search.py
@@ -3,7 +3,7 @@ class DSAsearch:
 	'''This module contains different types of searching funtions'''
 	
 	def __init__(self, arr):
-		self.arr = arr
+		self.arr = sorted(arr)
 	
 	def linear_search(self, target):
 		'''To search for the target in the array linearly'''
@@ -28,3 +28,24 @@ class DSAsearch:
 		
 		return -1 #Not found
 
+	def interpolation_search(self, target):
+		low, high = 0, len(self.arr) - 1
+
+		while low <= high and self.arr[low] <= target and self.arr[high] >= target:
+
+			if self.arr[low] == self.arr[high]:
+				if self.arr[low] == target:
+					return low
+				else:
+					return -1
+			
+			mid = low + ((target - self.arr[low]) * (high - low)) // (self.arr[high] - self.arr[low])
+
+			if self.arr[mid] == target:
+				return mid
+			elif self.arr[mid] < target:
+				low = mid + 1
+			else:
+				high = mid - 1
+		
+		return -1
--- Python/search.py
+++ Python/search.py
@@ -2,5 +2,6 @@ from dsa_search import DSAsearch
 
 arr1=DSAsearch([1, 2, 3, 4, 5])
 
-print(arr1.linear_search(6))
-print(arr1.binary_search(5))
+print(arr1.linear_search(4))
+print(arr1.binary_search(4))
+print(arr1.interpolation_search(4))
