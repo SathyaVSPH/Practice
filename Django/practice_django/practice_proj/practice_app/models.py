b76ac4818d3018ab5c6417fb36a6db01ebdcfdd1
AlienInvasion <vijayvsph@gmail.com>
2025-07-31 18:22:10 +0530

    modified models

--- Django/practice_django/practice_proj/practice_app/models.py
+++ Django/practice_django/practice_proj/practice_app/models.py
@@ -7,3 +7,6 @@ class Member(models.Model):
     f_name = models.CharField(max_length=255)
     l_name = models.CharField(max_length=255)
     age = models.IntegerField(null = True)
+
+    def __str__(self):
+        return f'Name: {self.f_name} {self.l_name}\nAge: {self.age}'
